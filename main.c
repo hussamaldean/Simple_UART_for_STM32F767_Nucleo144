00dc7bc018aa1b3e54c1a3167554b819f9a55d78
Hussam Aldean <31468020+hussamaldean@users.noreply.github.com>
2021-02-24 11:21:28 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include "stm32f7xx.h"                  // Device header
-volatile uint16_t i;
-
+//send data ti TX 
 void uart2_write(unsigned char x)
 {
 
 USART2->TDR =(x);
 while(!((USART2->ISR)&0x40)){;}
 }
-
+//if there is data iin the buffer
 int uart2_available(void)
 {
 if((USART2->ISR)&0x20){return 1;}
 else {return 0;}
 
 }
-unsigned char uart2_read(void)
+//read the RX buffer
+unsigned char uart2_read(void) 
 {
 return USART2->RDR;
 }
 
-
+//inefficient delay
 void delay(int delayms){
 int i;
 	for(; delayms>0;delayms--){
@@ -27,7 +27,7 @@ int i;
 	}
 }
 
-
+//blinking led algorithm 
 void led_play(int value)
 {
 	value %=16; 
@@ -47,21 +47,22 @@ SystemCoreClockUpdate();
 RCC->AHB1ENR  |= RCC_AHB1ENR_GPIOAEN;  // clock to AHBENR Port A and PA2 and PA3
 RCC->APB1ENR 	|= RCC_APB1ENR_USART2EN; // clock to USART2
 GPIOA->MODER 	|=0xA0 ; // PA2 and PA3 AF - PA5 Normal output
-GPIOA->MODER	|=0x400;
-GPIOA->OSPEEDR =0xFFFFFFF;
+GPIOA->MODER	|=0x400; //PA5 as output
+GPIOA->OSPEEDR =0xFFFFFFFF; //maximum speed for the entire GPIO port
 GPIOA->AFR[0] |= 0x7700; // PA2 and PA3 Set AF7
-USART2->BRR    = 0x008B; // baud rate 9600
+USART2->BRR    = 0x008B; // baud rate 115200 @16MHz
 USART2->CR1    = 0; // Enable Tx and Rx and Enable USART2
-USART2->CR1|=USART_CR1_TE;
-USART2->CR1|=USART_CR1_RE;	
+USART2->CR1|=USART_CR1_TE; //enbale TX
+USART2->CR1|=USART_CR1_RE; //enable RX	
 USART2->CR1|=USART_CR1_UE;
  while(1)
 {
 
-if(uart2_available())
+if(uart2_available()) //iif there is any data in RX-buffer
 		{
-		char c=uart2_read();
-			led_play(c);
+		char c=uart2_read(); //read the data
+			led_play(c);	//bliink the led
+			// print the sent character
 			uart2_write(c);
 			uart2_write('\r');
 			uart2_write('\n');
